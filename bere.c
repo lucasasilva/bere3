79e01a1d2b4b0ed3c273e18ec3111e1ef2ea5f3d
Lucas Silva <las.lucasantonio@gmail.com>
2024-05-16 17:02:09 -0300

    Testes
    Testes de referencia para bibliotecas personalizadas e organização da estrutura inicial do projeto.

---
+++ bere.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*Bibliotecas personalizadas*/
+#include "./bibliotecas/menus.h"
+
+void main () {
+    fMenuPrincipal();
+}
---
+++ bibliotecas/menus.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*----------------MENU PRINCIPAL-----------------------------------------------------*/
+void fMenuPrincipal()
+{
+    system("cls");
+    printf("==========================================\n");
+    printf("============      BERE'S      ============\n");
+    printf("==========================================\n");
+    printf("==        Escolha uma das opcoes        ==\n");
+    printf("== 1.   MATERIAIS DE LIMPEZA            ==\n");
+    printf("== 2.   ALIMENTOS E BEBIDAS             ==\n");
+    printf("== 3.   PADARIA                         ==\n");
+    printf("== 4.   FECHAR A VENDA                  ==\n");
+    printf("==                                      ==\n");
+    printf("== 5.   ABERTURA DE CAIXA               ==\n");
+    printf("== 6.   FECHAMENTO DE CAIXA             ==\n");
+    printf("==                                      ==\n");
+    printf("== 0.   ENCERRAR O DEADLOCKS - PDV      ==\n");
+    printf("==========================================\n");
+    printf("== Deadlocks Solucoes Empresariais      ==\n");
+    printf("==========================================\n");
+}
