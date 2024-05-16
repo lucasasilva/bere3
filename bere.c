9ee1653819f4673c40d5efe72d2770e4e3218122
Lucas Silva <las.lucasantonio@gmail.com>
2024-05-16 17:41:33 -0300

    testes
    testes com mudança de localidade para português

--- bere.c
+++ bere.c
@@ -2,9 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
+
 /*Bibliotecas personalizadas*/
 #include "./bibliotecas/menus.h"
 
-void main () {
+int main () {
+    system("chcp 65001");//Muda a págica de código dos consoles windows para UTF-8, fazendo com que o nosso idioma seja compreendido com seus acentos;
     fMenuPrincipal();
 }
--- bibliotecas/menus.h
+++ bibliotecas/menus.h
@@ -1,22 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 /*----------------MENU PRINCIPAL-----------------------------------------------------*/
 void fMenuPrincipal()
 {
+
+    system("chcp 65001");//Muda a págica de código dos consoles windows para UTF-8, fazendo com que o nosso idioma seja compreendido com seus acentos;
     system("cls");
     printf("==========================================\n");
     printf("============      BERE'S      ============\n");
     printf("==========================================\n");
-    printf("==        Escolha uma das opcoes        ==\n");
-    printf("== 1.   MATERIAIS DE LIMPEZA            ==\n");
-    printf("== 2.   ALIMENTOS E BEBIDAS             ==\n");
-    printf("== 3.   PADARIA                         ==\n");
-    printf("== 4.   FECHAR A VENDA                  ==\n");
+    printf("==        Escolha uma das opções        ==\n");
+    printf("== 1.   CADASTROS                       ==\n");
+    printf("== 2.   VENDAS                          ==\n");
+    printf("== 3.   ABERTURA DE CAIXA               ==\n");
+    printf("== 4.   FECHAMENTO DE CAIXA             ==\n");
     printf("==                                      ==\n");
-    printf("== 5.   ABERTURA DE CAIXA               ==\n");
-    printf("== 6.   FECHAMENTO DE CAIXA             ==\n");
+    printf("== 5.   RELATÓRIOS                      ==\n");
     printf("==                                      ==\n");
-    printf("== 0.   ENCERRAR O DEADLOCKS - PDV      ==\n");
+    printf("== 7.   ENCERRAR O DEADLOCKS - PDV      ==\n");
     printf("==========================================\n");
     printf("== Deadlocks Solucoes Empresariais      ==\n");
     printf("==========================================\n");
