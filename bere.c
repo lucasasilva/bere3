b3ffe75f4c6c44c524aaf047d558ee177d77118c
Lucas Silva <las.lucasantonio@gmail.com>
2024-05-20 16:29:32 -0300

    Produtos de teste, consulta produtos cadastrados
    1. Iniciados os produtos usados para teste no main.c
    2. Implementado método para retornar produtos  cadastrados, no structCadastros.h.
    3. Movido método "retornaCategoria" para o arquivo "structsCadastros.h"(Estava em "utilidades.h")

--- bere.c
+++ bere.c
@@ -13,17 +13,40 @@
 int main () {
     system("chcp 65001");//Muda a págica de código dos consoles windows para UTF-8, fazendo com que o nosso idioma seja compreendido com seus acentos;
     int vMenu;
-    Terceiros *cliente= (Terceiros *) calloc(1, sizeof(Terceiros));
-    SaldosVendas *saldosVendas= (SaldosVendas *) calloc(1, sizeof(SaldosVendas));
-    Produtos *produto= (Produtos *) calloc(3, sizeof(Produtos));//alocando espaço para 3 produtos, porque é o que teremos visível para teste
-    produto[0].codigoProduto = 1000;
-    strcpy(produto[0].nomeProduto, "Pão Francês");
-    produto[0].categoria = 1;
-    produto[0].custoProduto = 0.80;
-    produto[0].margemLucro = 80.00;
+    int vInputUsuario;
+    int vAlocacaoMemoriaCliente = 1;
+    int vAlocacaoMemoriaVendas = 1;
+    int vAlocacaoMemoriaProdutos = 3;//alocando espaço para 3 produtos, porque é o que teremos visível para teste
 
-    fRetornaProdutosCadastros(produto, 3, fREtornaCategoria(produto->categoria));
-    fMenuPrincipal();
+    Terceiros *cliente= (Terceiros *) calloc(vAlocacaoMemoriaCliente, sizeof(Terceiros));
+    SaldosVendas *saldosVendas= (SaldosVendas *) calloc(vAlocacaoMemoriaVendas, sizeof(SaldosVendas));
+    Produtos *produto= (Produtos *) calloc(vAlocacaoMemoriaProdutos, sizeof(Produtos));
+
+    /*Produtos demonstração;*/
+    produto[0].codigoProduto = 1000;
+    strcpy(produto[0].nomeProduto, "Detergente");
+    produto[0].categoria = 1;
+    produto[0].custoProduto = 01.20;
+    produto[0].margemLucro = 62.50;
+    produto[0].qtdEstoque = 2;
+
+    produto[1].codigoProduto = 1001;
+    strcpy(produto[1].nomeProduto, "Café");
+    produto[1].categoria = 2;
+    produto[1].custoProduto = 14.80;
+    produto[1].margemLucro = 40.00;
+    produto[1].qtdEstoque = 5;
+    
+    produto[2].codigoProduto = 1002;
+    strcpy(produto[2].nomeProduto, "Pão Francês");
+    produto[2].categoria = 3;
+    produto[2].custoProduto = 00.80;
+    produto[2].margemLucro = 80.00;
+    produto[2].qtdEstoque = 10;
+
+
+    fRetornaCadastrosProdutos(produto, vAlocacaoMemoriaProdutos);
+    //fMenuPrincipal();
     scanf("%d", &vMenu);
     switch (vMenu)
     {
@@ -35,6 +58,7 @@ int main () {
         break;
     
     default:
+        main();
         break;
     }
 
--- bibliotecas/utilidades.h
+++ bibliotecas/utilidades.h
@@ -2,19 +2,3 @@
 #include <string.h>
 char* fREtornaCategoria(int categoria);
 /*Retorna o nome da categoria do produto, para fins de impressão no console*/
-char* fREtornaCategoria(int categoria){
-    switch (categoria)
-    {
-    case 1:
-        return "Material de Limpeza";
-        break;
-    case 2:
-        return "alimento";
-        break;
-    case 3:
-        return "Padaria";
-        break;
-    default:
-        break;
-    }
-}
