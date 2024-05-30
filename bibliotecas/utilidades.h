8309f291d9c6cf899f3d76cd30f2b5935ba0affd
Lucas Silva <las.lucasantonio@gmail.com>
2024-05-30 15:33:42 -0300

    Criadas duas funções para apoio ao cadastro de clientes
    Criada função "fRetornaEstadoCliente", para que seja possível informar um integer no cadastro do cliente e retornar o nome do estado da pessoa no momento da visualização;
    Criada função "fImprimeEstadosTela", para visualização dos  códigos dos estados no momento do cadastro do cliente;

--- bibliotecas/utilidades.h
+++ bibliotecas/utilidades.h
@@ -1,4 +1,60 @@
+#ifndef utilidades_h
+#define utilidades_h
+
 #include <stdio.h>
 #include <string.h>
-char* fREtornaCategoria(int categoria);
-/*Retorna o nome da categoria do produto, para fins de impressão no console*/
+
+const char* fRetornaEstadoCliente(int numero) {
+    switch (numero) {
+        case 1: return "Acre";
+        case 2: return "Alagoas";
+        case 3: return "Amapá";
+        case 4: return "Amazonas";
+        case 5: return "Bahia";
+        case 6: return "Ceará";
+        case 7: return "Distrito Federal";
+        case 8: return "Espírito Santo";
+        case 9: return "Goiás";
+        case 10: return "Maranhão";
+        case 11: return "Mato Grosso";
+        case 12: return "Mato Grosso do Sul";
+        case 13: return "Minas Gerais";
+        case 14: return "Pará";
+        case 15: return "Paraíba";
+        case 16: return "Paraná";
+        case 17: return "Pernambuco";
+        case 18: return "Piauí";
+        case 19: return "Rio de Janeiro";
+        case 20: return "Rio Grande do Norte";
+        case 21: return "Rio Grande do Sul";
+        case 22: return "Rondônia";
+        case 23: return "Roraima";
+        case 24: return "Santa Catarina";
+        case 25: return "São Paulo";
+        case 26: return "Sergipe";
+        case 27: return "Tocantins";
+        default: return "Não Cadastrado/Estrangeiro.";
+    }
+}
+
+void fImprimeEstadosTela (){
+    for (int i = 0; i < 28; i++)
+    {
+        printf("%d. %-9s\t",i,fRetornaEstadoCliente(i)); 
+        if (i ==8)
+        {
+            printf("\n"); 
+        }
+        if (i ==16)
+        {
+            printf("\n"); 
+        } 
+        if (i ==24)
+        {
+            printf("\n"); 
+        }       
+    }
+}
+
+
+#endif
